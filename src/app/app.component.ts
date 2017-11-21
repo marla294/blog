8f5da4a5dcce9476cc91e7a9e4fa60873fdd04b9
Marla Foreman <marlaforeman@marlas-mbp.lan>
2017-11-21 09:54:41 -0600

    Lots o' stuff - setting up router
    - Created Home component and moved post links into there
    - Created post component
    - Completely set up the router
    - Copied router animations from last website project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, animate, transition, animateChild }	from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+  	trigger('routerAnimation', [
+  		transition('* <=> *', [
+  			style({opacity: 0}), animate(500)
+  		])
+  	])
+  ]
 })
 export class AppComponent {
-  title = "Marla's Blog";
+  	getRoute(outlet) {
+	    return outlet.activatedRouteData.state;
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,34 @@
-import { BrowserModule } 			from '@angular/platform-browser';
-import { NgModule } 				from '@angular/core';
-import { BrowserAnimationsModule }	from '@angular/platform-browser/animations';
-import { MatListModule }			from '@angular/material';
+import { BrowserModule } 			        from '@angular/platform-browser';
+import { NgModule } 				          from '@angular/core';
+import { BrowserAnimationsModule }    from '@angular/platform-browser/animations'
+import { RouterModule, Routes }       from '@angular/router';
+import { MatListModule }			        from '@angular/material';
 
-import { AppComponent } 			from './app.component';
-import { HeaderComponent }			from './header.component';
-import { FooterComponent }			from './footer.component';
+import { AppComponent } 			        from './app.component';
+import { HomeComponent }              from './home.component';
+import { HeaderComponent }			      from './header.component';
+import { FooterComponent }			      from './footer.component';
+import { PostComponent }              from './post.component';
 
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent, data: { state: 'home' } },
+  { path: 'post', component: PostComponent, data: { state: 'post' } },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatListModule
+    MatListModule,
+    RouterModule.forRoot(appRoutes, {useHash: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
