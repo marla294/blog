e7f65ec038ebbc8db7a9c57bc716b0d4948358fe
Marla Foreman <marlaforeman@marlas-mbp.lan>
2017-11-24 17:03:48 -0600

    Removing files that are confusing angular-cli

--- src/app/post.directive.ts
+++
@@ -1,8 +0,0 @@
-import { Directive, ViewContainerRef }		from '@angular/core';
-
-@Directive({
-	selector: '[post-host]',
-})
-export class PostDirective {
-	constructor(public viewContainerRef: ViewContainerRef) { }
-}
