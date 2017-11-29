de893ac3a11d3c110a7caa3540bc7197d559d7c7
Marla Foreman <marlaforeman@marlas-mbp.lan>
2017-11-29 10:13:29 -0600

    Checking in my "bug tracking system"

--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -2,7 +2,6 @@ import { Injectable }		from '@angular/core';
 import { Observable }		from 'rxjs/Observable';
 import { HttpClient }		from '@angular/common/http';
 import 'rxjs/add/observable/of';
-import 'rxjs/add/operator/map';
 
 import { Post }				from './post';
 
