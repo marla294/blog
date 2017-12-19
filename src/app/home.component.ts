5815bc7daff9ee377f978369d192cfd81de04a01
Marla Foreman <marlaforeman@marlas-mbp.lan>
2017-12-19 15:43:33 -0600

    Adding month/year headers on home page by month/year
    - Added month/year headers in home component
    - Added monthYear to post class
    - Moved home component html to its own file

--- src/app/home.component.ts
+++ src/app/home.component.ts
@@ -10,31 +10,23 @@ import { Post }					from './post/post';
 
 @Component({
 	selector: 'home',
-	template: `
-	<mat-list class="post-list container">
-	  	<mat-list-item *ngFor="let post of posts$ | async">
-	  		<a matLine (click)=goToPost(post)>{{post.title}}</a>
-	  		<p matLine><i>{{post.date | date}}</i></p>
-	  	</mat-list-item>
-	  <div *ngIf="posts$ | async as posts">
-	  	<a mat-list-item *ngIf="posts.length < postsLength" (click)="showMore(posts.length)">Show More...</a>
-	  </div>
-	</mat-list>
-	`,
+	templateUrl: './home.component.html',
 	styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 	//List of posts to show on the page
 	posts$: Observable<Post[]>;
 	postsLength: number;
+	monthYear: string[];
 
 	/* -- Variables for arranging the array of posts on the home page -- */
 	//Variable for sort order
-	sortOrder: string = 'idDesc'; //What to sort posts by and what order
+	sortOrder: string = 'dateAsc'; //What to sort posts by and what order
 
 	//Variables for "show more" functionality
 	defaultPostsPerPage: number = 10; //How many posts to show per page
 	showMoreAmount: number = 10; //The amount of additional posts to show when button clicked
+
 	constructor(
 		private service: PostService,
 		private router: Router
@@ -42,7 +34,10 @@ export class HomeComponent implements OnInit {
 
 	ngOnInit() {
 		this.posts$ = this.service.getPostsWithOperators(this.sortOrder, this.defaultPostsPerPage);
-		this.service.getPosts().subscribe(posts => this.postsLength = posts.length);
+		this.service.getPosts().subscribe(posts => {
+			this.postsLength = posts.length;
+			this.monthYear = this.getMonthYearGroups(posts);
+		});
 	}
 
 	goToPost(post: Post) {
@@ -53,4 +48,15 @@ export class HomeComponent implements OnInit {
 		this.posts$ = this.service.getPostsWithOperators(this.sortOrder, currentLength + this.showMoreAmount);
 	}
 
+	getMonthYearGroups(posts: Post[]): string[] {
+		let newPosts = posts.map(post => post.monthYear);
+		let newArr: string[] = [];
+		newPosts.map((curr) => {
+			if (newArr.findIndex(d => d == curr) == -1) {
+				newArr.push(curr);
+			}
+		});
+		return newArr;
+	}
+	
 }
--- src/app/post/post.ts
+++ src/app/post/post.ts
@@ -1,6 +1,15 @@
 export class Post {
 	date: Date;
+	monthYear: string;
 	constructor(public id: string, public title: string, public dateStr: string) {
 		this.date = new Date(dateStr);
+		/*this.monthYear = this.date.getMonth().toString() + this.date.getFullYear().toString()*/
+		this.monthYear = this.getMonthFullName(this.date.getMonth()) + ' ' + this.date.getFullYear().toString()
+	}
+
+	getMonthFullName(month: number): string {
+		var monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"];
+  		return monthNames[month];
 	}
 }
