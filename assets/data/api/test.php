6187867e8d349b41f56b099f8570be888df727ec
Marla Foreman <marlaforeman@marlas-mbp.lan>
2018-02-13 22:56:19 -0600

    Auto-generated commit

--- assets/data/api/test.php
+++ assets/data/api/test.php
@@ -1,9 +1,14 @@
 <?php
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Headers: *');
-header('Content-Type: application/x-www-form-urlencoded');
-header('Content-Type: application/json');
 
-	$data = "hi";
-	echo json_encode($data);
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+//header('Content-Type: text/plain');
+
+if ($_POST['name'] || $_POST['email'] || $_POST['body']) {
+	echo $_POST['name'];
+	echo $_POST['email'];
+	echo $_POST['body'];
+}
+
+
 ?>
