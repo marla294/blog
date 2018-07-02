fd3cba0c2472b902b577e25320fa73703ef9064f
Marla Foreman <marlaforeman@marlas-mbp.lan>
2018-07-02 00:55:29 -0500

    Post 14
    No idea why the email component got updated.

--- src/app/email.component.ts
+++ src/app/email.component.ts
@@ -1,23 +1,25 @@
-import { Component }								from '@angular/core';
-import { FormBuilder, FormGroup, Validators }		from '@angular/forms';
-import { HttpClient, HttpHeaders }					from '@angular/common/http';
+import { Component } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 @Component({
-	selector: 'email',
-	templateUrl: './email.component.html',
-	styleUrls: ['./email.component.css']
+	selector: "email",
+	templateUrl: "./email.component.html",
+	styleUrls: ["./email.component.css"]
 })
 export class EmailComponent {
 	emailForm: FormGroup;
 
-	headers = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+	headers = new HttpHeaders().set(
+		"Content-Type",
+		"application/x-www-form-urlencoded; charset=UTF-8"
+	);
 	data: any;
-	dataPost = { 
+	dataPost = {
 		name: "",
 		email: "",
 		body: ""
 	};
-
 
 	constructor(private fb: FormBuilder, private http: HttpClient) {
 		this.createForm();
@@ -29,34 +31,45 @@ export class EmailComponent {
 
 	createForm() {
 		this.emailForm = this.fb.group({
-			senderName: ['', Validators.required],
-			senderEmail: ['', Validators.email],
-			messageBody: ['', Validators.minLength(1)]
-		})
+			senderName: ["", Validators.required],
+			senderEmail: ["", Validators.email],
+			messageBody: ["", Validators.minLength(1)]
+		});
 	}
 
 	onSubmit() {
-		console.log('Submitted');
+		console.log("Submitted");
 	}
 
 	/* PHP */
 
 	prepDataForPost() {
-		this.dataPost.name = 'name=' + JSON.stringify(this.emailForm.controls.senderName.value);
-		this.dataPost.email = 'email=' + JSON.stringify(this.emailForm.controls.senderEmail.value);
-		this.dataPost.body = 'body=' + JSON.stringify(this.emailForm.controls.messageBody.value);
+		this.dataPost.name =
+			"name=" + JSON.stringify(this.emailForm.controls.senderName.value);
+		this.dataPost.email =
+			"email=" +
+			JSON.stringify(this.emailForm.controls.senderEmail.value);
+		this.dataPost.body =
+			"body=" + JSON.stringify(this.emailForm.controls.messageBody.value);
 	}
-
 
 	onDataSet() {
 		this.prepDataForPost();
 
-		this.http.post('http://localhost:7000', this.dataPost.name, {headers: this.headers})
+		this.http
+			.post("http://localhost:7000", this.dataPost.name, {
+				headers: this.headers
+			})
 			.subscribe(res => console.log(res));
-		this.http.post('http://localhost:7000', this.dataPost.email, {headers: this.headers})
+		this.http
+			.post("http://localhost:7000", this.dataPost.email, {
+				headers: this.headers
+			})
 			.subscribe(res => console.log(res));
-		this.http.post('http://localhost:7000', this.dataPost.body, {headers: this.headers})
+		this.http
+			.post("http://localhost:7000", this.dataPost.body, {
+				headers: this.headers
+			})
 			.subscribe(res => console.log(res));
 	}
-
 }
